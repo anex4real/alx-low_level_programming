f29dd0359f535590ac15196dd10b51910f3f1159
anex4real <anex4real@gmail.com>
2022-10-19 01:55:11 -0700

    task 12.5

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,9 +1,8 @@
 #include "main.h"
-
 /**
- * print_times_table - prints the times table up to the integer input
- * @n: integer input
- * Return: void
+ * print_times_table - Entry point
+ * @n: input
+ * Return: Always 0 (Success)
  */
 
 void print_times_table(int n)
