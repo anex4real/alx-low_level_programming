34ecd03f99bebfb0ba3d914b05d595949ac6c79d
anex4real <anex4real@gmail.com>
2022-10-19 04:55:49 -0700

    task 13

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Print all multiples of 3 & 9 below 1024
+ *
+ * Return: Return 0
+ */
+int main(void)
+{
+	int n, x;
+	int n, sum;
+
+	while (n < 1024)
+	{
+		if ((n % 3) == 0 || (n % 5) == 0)
+		{
+			x += n;
+			sum += n;
+		}
+		n++;
+	}
+	printf("%d\n", n);
+	printf("%d\n", sum);
+	return (0);
+}
